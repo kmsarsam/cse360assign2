dae44b67d80dad13afe73725bb2a7a7f122e16a4
Kamila Sarsam <kmsarsam@gmail.com>
2019-10-08 20:38:53 -0700

    first commit

---
+++ src/cse360assign2/AddingMachine.java
@@ -0,0 +1,36 @@
+/* Author: Kamila Sarsam
+ * Class ID: CSE 360
+ * Assignment: 1
+ * Description: This class takes in a string and determines whether 
+ * it's a palindrome or not */
+
+package cse360assign2;
+
+public class AddingMachine {
+
+	private int total;
+	
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	public void add (int value) {
+		
+	}
+	
+	public void subtract (int value) {
+		
+	}
+		
+	public String toString () {
+		return "";
+	}
+
+	public void clear() {
+	
+	}
+}
